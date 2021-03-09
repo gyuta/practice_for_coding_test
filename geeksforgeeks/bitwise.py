222398a9fe02ed7ebf1e1dbd510c56229aaf5678
gyuta <kuroshiba0408@gmail.com>
2021-03-09 23:09:25 +0900

    少し計算量を意識して書き換え

--- geeksforgeeks/bitwise.py
+++ geeksforgeeks/bitwise.py
@@ -4,15 +4,14 @@ import functools
 arr = [5,1,3,4,2]
 # arr = [2,6,2,8,4,5]
 
-dic = {}
+_max = functools.reduce(lambda x, y: x|y, arr)
+flg = False
 for i in range(1, len(arr)+1):
   for conb in itertools.combinations(arr, i):
     bor = functools.reduce(lambda x, y: x|y, conb)
-    print(conb, bor, dic)
-    if bor not in dic:
-      dic[bor] = i
-
-_max = functools.reduce(lambda x, y: x|y, arr)
-print(_max)
-print(dic[_max])
-    
+    if _max == bor:
+      print(i)
+      flg = True
+      break
+  if flg:
+    break
