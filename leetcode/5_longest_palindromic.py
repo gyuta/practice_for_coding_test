6f17defb1b3e1c9e141df22e907dc023c043e0a2
gyuta <kuroshiba0408@gmail.com>
2021-03-11 23:42:46 +0900

    5 愚直な解法

--- leetcode/5_longest_palindromic.py
+++ leetcode/5_longest_palindromic.py
@@ -1,5 +1,8 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        if len(s) == 1:
+          return s[0]
+
         # 2
         count = 0
         ans = ""
@@ -16,11 +19,12 @@ class Solution:
                         break
                     if s[l] != s[r]:
                         break
-                    count += 1
+                    count += 2
                     l -= 1
                     r += 1
                 if count > _max:
-                    ans = s[l+1, r]
+                    ans = s[l+1: r]
+                    _max = len(ans)
         for i in range(1, len(s) -1):
             if s[i-1] == s[i+1]:
                 count = 3
@@ -33,8 +37,15 @@ class Solution:
                         break
                     if s[l] != s[r]:
                         break
-                    count += 1
+                    count += 2
                     l -= 1
                     r += 1
                 if count > _max:
-                    ans = s[l+1, r]
+                    ans = s[l+1: r]
+                    _max = len(ans)
+        return ans
+
+if __name__ == "__main__":
+  s = Solution()
+  ans = s.longestPalindrome("aaaa")
+  print(ans)
